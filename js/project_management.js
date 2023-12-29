11295acccb91bbf33d3f7efeb2b3bfaeecac363b
kj <kkjiang27@gmail.com>
2023-12-28 21:36:09 -0500

    Project Display Structure

--- js/project_management.js
+++ js/project_management.js
@@ -3,31 +3,53 @@
 // Author: Kuang Jiang
 // Description: JavaScript code for sorting and arranging projects
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Get all project containers and convert to an array
-    var projects = document.querySelectorAll(".project-container");
+// Function to load HTML content
+async function loadHTML(url) {
+    try {
+        const response = await fetch(url);
+        const html = await response.text();
+        const parser = new DOMParser();
+        return parser.parseFromString(html, 'text/html');
+    } catch (error) {
+        console.error('Error loading HTML:', error);
+        return null;
+    }
+}
 
-    // Sort the projects based on the year in descending order
-    projects = Array.from(projects).sort(function (a, b) {
-        var yearA = parseInt(a.querySelector(".arrow-pointer").textContent);
-        var yearB = parseInt(b.querySelector(".arrow-pointer").textContent);
-        return yearB - yearA;
-    });
+// Function to execute after HTML is loaded
+async function main() {
+    // Load all_projects.html content
+    const allProjectsHTML = await loadHTML('../html/all_projects.html');
 
-    // Clear existing content
-    var sortedProjectsContainer = document.getElementById("sorted-projects-container");
-    sortedProjectsContainer.innerHTML = "";
+    if (allProjectsHTML) {
+        // Get all project containers from the loaded HTML
+        var projects = allProjectsHTML.querySelectorAll(".project-container");
 
-    // Append sorted projects to the container
-    projects.forEach(function (project, index) {
-        // Create a new row for every three projects
-        if (index % 3 === 0) {
-            var row = document.createElement("div");
-            row.className = "project-row";
-            sortedProjectsContainer.appendChild(row);
-        }
+        // Sort the projects based on the year in descending order
+        projects = Array.from(projects).sort(function (a, b) {
+            var yearA = parseInt(a.querySelector(".arrow-pointer").textContent);
+            var yearB = parseInt(b.querySelector(".arrow-pointer").textContent);
+            return yearB - yearA;
+        });
 
-        // Append the project container to the current row
-        sortedProjectsContainer.lastElementChild.appendChild(project.cloneNode(true));
-    });
-});
+        // Clear existing content in #sorted-projects-container
+        var sortedProjectsContainer = document.getElementById("sorted-projects-container");
+        sortedProjectsContainer.innerHTML = "";
+
+        // Append sorted projects to the container
+        projects.forEach(function (project, index) {
+            // Create a new row for every three projects
+            if (index % 3 === 0) {
+                var row = document.createElement("div");
+                row.className = "project-row";
+                sortedProjectsContainer.appendChild(row);
+            }
+
+            // Append the project container to the current row
+            sortedProjectsContainer.lastElementChild.appendChild(project.cloneNode(true));
+        });
+    }
+}
+
+// Call the main function when the DOM is ready
+document.addEventListener("DOMContentLoaded", main);
