d52405c469c004a45166be0887331a12a7ab331e
kj <kkjiang27@gmail.com>
2024-01-03 12:37:50 -0500

    Global Header

--- script.js
+++ script.js
@@ -3,6 +3,18 @@
 // Author:  Kuang Jiang
 // Description: This is the script file for the index.html file.  It contains the functions that are called by the index.html file.
 
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Load the global header
+    loadGlobalHeader();
+
+    // Update the current year in the footer
+    updateCopyrightYear();
+    // Set up an interval to update the year every minute
+    setInterval(updateCopyrightYear, 60000); // 60000 milliseconds = 1 minute
+});
+
+
 // Script for back-to-top button
 window.onscroll = function() {
     scrollFunction();
@@ -29,7 +41,35 @@ function updateCopyrightYear() {
     copyrightElement.textContent = "Copyright © 2015-" + currentYear + " Kuang Jiang. All rights reserved.";
 }
 
-updateCopyrightYear();
+// Define the async function to load HTML
+async function loadHTML(url) {
+    try {
+        const response = await fetch(url);
+        const html = await response.text();
+        const parser = new DOMParser();
+        return parser.parseFromString(html, 'text/html');
+    } catch (error) {
+        console.error('Error loading HTML:', error);
+        return null;
+    }
+}
 
-// Set up an interval to update the year every minute
-setInterval(updateCopyrightYear, 60000); // 60000 milliseconds = 1 minute
+// Function to execute after HTML is loaded
+async function loadGlobalHeader() {
+    // Load all_projects.html content
+    const headerHTML = await loadHTML('html/global_header.html');
+
+    if (headerHTML) {
+        // Get all project containers from the loaded HTML
+        var globalHeader = headerHTML.querySelectorAll(".header-container");
+
+        // Clear existing content in #sorted-projects-container
+        var headerContainer = document.getElementById("globalHeader");
+        headerContainer.innerHTML = "";
+
+        // Add the loaded HTML to the page
+        globalHeader.forEach(function (header) {
+            headerContainer.appendChild(header);
+        });
+    }
+}
