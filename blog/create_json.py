d173ece83eec6a5578ee0a01c746664d4527cea5
kj <kkjiang27@gmail.com>
2024-01-14 10:49:34 -0500

    Blog Page Content Update

--- blog/create_json.py
+++ blog/create_json.py
@@ -13,14 +13,21 @@ def extract_title(html_content):
     title_div = soup.find('div', {'class': 'title'})
     return title_div.text.strip() if title_div else None
 
+def extract_publish_time(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    publish_time_span = soup.find('span', {'class': 'publish-time'})
+    return publish_time_span.text.strip() if publish_time_span else None
+
 def process_html_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         html_content = file.read()
         title = extract_title(html_content)
-        if title:
+        publish_time = extract_publish_time(html_content)
+        if title and publish_time:
             return {
                 'title': title,
-                'url': f'/blog/{os.path.basename(file_path)}'
+                'url': f'/blog/{os.path.basename(file_path)}',
+                'publish_time': publish_time
             }
         else:
             return None
@@ -28,8 +35,8 @@ def process_html_file(file_path):
 def main():
     html_files = glob.glob('*.html')
 
-    # Sort files based on creation time
-    html_files.sort(key=lambda x: os.path.getctime(x))
+    # Sort files in descending order based on the content within the <span class='publish-time'> tag
+    html_files.sort(key=lambda x: datetime.strptime(extract_publish_time(open(x).read()), '%B %Y') if extract_publish_time(open(x).read()) else datetime.min, reverse=True)
 
     result = []
 
